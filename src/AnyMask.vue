12dd6a3210e4b6cc333007fb527321b358654c6a
VT001 <VT001@qq.com>
2025-07-23 10:18:49 +0800

    暂存

---
+++ src/AnyMask.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mask" v-if="maskVisable">
+    <div class="loader">
+      <div class="loading-1"></div>
+      <div class="loading-2">{{ text }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AnyMask",
+  props: {
+    text: {
+      type: [String, Number],
+      default: "",
+    },
+    maskVisable: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+<style scoped>
+  
+</style>
---
+++ src/index.ts
@@ -0,0 +1,27 @@
+/*
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2025-07-23 09:39:17
+ * @LastEditTime: 2025-07-23 09:55:11
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description: 
+ * @FilePath: \LoadingMask\src\index.ts
+ * 
+ */
+/**
+ * @format
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2021-11-16 10:55:32
+ * @LastEditTime: 2021-11-16 11:12:42
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description:
+ * @FilePath: \LoadingMask\src\index.js
+ */
+
+import loadingMask from "./LoadingMask.vue";
+
+import anyMask from "./AnyMask.vue";
+
+export const AnyMask = anyMask;
+export const LoadingMask = loadingMask;
